66cebae91bce089343c53c4912614b390d2abca1
Quindart <quang.le@aegona.com>
2025-04-16 16:56:13 +0700

    feat: useHookFriend

--- src/hooks/useFriend.ts
+++ src/hooks/useFriend.ts
@@ -1,27 +1,187 @@
-import { Alert } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
-import axiosConfig from '@/services/axios.config';
-import { setMe } from '@/redux/userSlice';
-import { NavigationProp, ParamListBase, useNavigation } from '@react-navigation/native';
-import { ROUTING } from '@/utils/constant';
-import { RootState } from '@/redux/store';
-import { User } from '@/models/user';
-import { getFriendsList } from '@/services/friend.service';
+import { RootState } from "@/redux/store";
+import { setReceiveFriends, setSendFriends } from "@/redux/userSlice";
+import SocketService from "@/services/Socket.service";
+import { useEffect, useState, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-const useFriend = () => {
-    const getAllFriend = async () => {
-        try {
-            console.log(' Danh sách bạn bè:');
-            const response = await getFriendsList();
-            console.log('B1: Danh sách bạn bè:', response.data);
-            console.log('B2: Danh sách bạn bè:', response.data.friends);
-            return response.data.friends;
-        } catch (error) {
-            console.error('Error fetching friends list:', error);
-            throw error;
-        }
-    }
-    return { getAllFriend };
+const SOCKET_EVENTS = {
+    FRIEND: {
+        ADD_FRIEND: "friend:add",
+        ADD_FRIEND_RESPONSE: "friend:addResponse",
+        REMOVE_FRIEND: "friend:remove",
+        REMOVE_FRIEND_RESPONSE: "friend:removeResponse",
+        ACCEPT_FRIEND: "friend:accept",
+        ACCEPT_FRIEND_RESPONSE: "friend:acceptResponse",
+        REJECT_FRIEND: "friend:reject",
+        REJECT_FRIEND_RESPONSE: "friend:rejectResponse",
+        REVOKE_FRIEND: "friend:revoke",
+        REVOKE_FRIEND_RESPONSE: "friend:revokeResponse",
+
+        LIST_FRIEND: "friend:list",
+        LIST_FRIEND_RESPONSE: "friend:listResponse",
+
+        LIST_SEND_INVITE: "friend:listSendInvite",
+        LIST_SEND_INVITE_RESPONSE: "friend:listSendInviteResponse",
+
+        LIST_RECEIVED_INVITE: "friend:listReceviedInvite",
+        LIST_RECEIVED_INVITE_RESPONSE: "friend:listReceviedInviteResponse",
+    },
+};
+
+interface ResponseType {
+    success: boolean;
+    message: string;
+    data: any;
 }
 
-export default useFriend;
+export const useFriend = (currentUserId: string) => {
+    //TODO: store
+    const userStore = useSelector((state: RootState) => state.user)
+    const { receiveFriends, sendFriends } = userStore
+
+    const dispatch = useDispatch();
+    const [listFriends, setListFriends] = useState<any[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    const socketService = SocketService.getInstance(currentUserId).getSocket();
+
+    useEffect(() => {
+        if (!socketService.connected) {
+            socketService.connect();
+        }
+        const listFriendsResponse = (response: ResponseType) => {
+            if (response.success) {
+                setListFriends(response.data);
+                setLoading(false);
+            } else {
+                console.error("Failed to load friend list:", response.message);
+            }
+        };
+        const inviteListFriendsResponse = (response: ResponseType) => {
+            if (response.success) {
+                dispatch(setReceiveFriends(response.data));
+            } else {
+                console.error("Failed to load invite friend list:", response.message);
+            }
+        };
+
+        const sendListFriendsResponse = (response: ResponseType) => {
+            if (response.success) {
+                dispatch(setSendFriends(response.data));
+            } else {
+                console.error("Failed to load invite friend list:", response.message);
+            }
+        };
+
+
+        const handleResponseFromFriendEvent = (response: ResponseType) => {
+            if (!(response.success && response.data)) {
+                return
+            }
+            console.log("💲💲💲 ~ handleResponseFromFriendEvent ~  response.data:", response.data)
+
+            if (Object.prototype.hasOwnProperty.call(response.data, "receiverList")) {
+                dispatch(setReceiveFriends(response.data.receiverList));
+            }
+
+            else if (Object.prototype.hasOwnProperty.call(response.data, "senderList")) {
+                dispatch(setSendFriends(response.data.senderList));
+            }
+
+            if (Object.prototype.hasOwnProperty.call(response.data, "friends")) {
+                setListFriends(response.data.friends);
+            }
+        }
+
+        socketService.on(SOCKET_EVENTS.FRIEND.LIST_FRIEND_RESPONSE, listFriendsResponse);
+        socketService.on(SOCKET_EVENTS.FRIEND.LIST_RECEIVED_INVITE_RESPONSE, inviteListFriendsResponse);
+        socketService.on(SOCKET_EVENTS.FRIEND.LIST_SEND_INVITE_RESPONSE, sendListFriendsResponse);
+
+
+        socketService.on(SOCKET_EVENTS.FRIEND.ADD_FRIEND_RESPONSE, handleResponseFromFriendEvent);
+        socketService.on(SOCKET_EVENTS.FRIEND.ACCEPT_FRIEND_RESPONSE, handleResponseFromFriendEvent);
+        socketService.on(SOCKET_EVENTS.FRIEND.REJECT_FRIEND_RESPONSE, handleResponseFromFriendEvent);
+        socketService.on(SOCKET_EVENTS.FRIEND.REVOKE_FRIEND_RESPONSE, handleResponseFromFriendEvent);
+
+        return () => {
+            socketService.off(SOCKET_EVENTS.FRIEND.LIST_FRIEND_RESPONSE, listFriendsResponse);
+            socketService.off(SOCKET_EVENTS.FRIEND.LIST_RECEIVED_INVITE_RESPONSE, inviteListFriendsResponse);
+            socketService.off(SOCKET_EVENTS.FRIEND.LIST_SEND_INVITE_RESPONSE, sendListFriendsResponse);
+
+            socketService.off(SOCKET_EVENTS.FRIEND.ADD_FRIEND_RESPONSE, handleResponseFromFriendEvent);
+            socketService.off(SOCKET_EVENTS.FRIEND.ACCEPT_FRIEND_RESPONSE, handleResponseFromFriendEvent);
+            socketService.off(SOCKET_EVENTS.FRIEND.REJECT_FRIEND_RESPONSE, handleResponseFromFriendEvent);
+            socketService.off(SOCKET_EVENTS.FRIEND.REVOKE_FRIEND_RESPONSE, handleResponseFromFriendEvent);
+
+        };
+    }, [socketService]);
+
+
+    const getListFriends = useCallback(() => {
+        setLoading(true);
+        const params = {
+            userId: currentUserId,
+        }
+        socketService.emit(SOCKET_EVENTS.FRIEND.LIST_FRIEND, params);
+    }, [currentUserId]);
+
+
+    const getReceviedInviteFriends = useCallback(() => {
+        socketService.emit(SOCKET_EVENTS.FRIEND.LIST_RECEIVED_INVITE, { userId: currentUserId });
+    }, []);
+
+    const getSendListFriends = useCallback(() => {
+        socketService.emit(SOCKET_EVENTS.FRIEND.LIST_SEND_INVITE, { userId: currentUserId });
+    }, [currentUserId]);
+
+    const inviteFriend = useCallback((userFriendId: string) => {
+        const params = {
+            userId: currentUserId,
+            userFriendId: userFriendId,
+        }
+        socketService.emit(SOCKET_EVENTS.FRIEND.ADD_FRIEND, params);
+    }, [currentUserId]);
+
+    const rejectInviteFriend = useCallback((userFriendId: string) => {
+        socketService.emit(SOCKET_EVENTS.FRIEND.REJECT_FRIEND, {
+            userId: currentUserId,
+            userFriendId: userFriendId,
+        });
+    }, [currentUserId])
+
+    const revokeInviteFriend = useCallback((userFriendId: string) => {
+        socketService.emit(SOCKET_EVENTS.FRIEND.REVOKE_FRIEND, {
+            userId: currentUserId,
+            userFriendId: userFriendId,
+        });
+    }, [currentUserId])
+
+    const removeFriend = useCallback((userFriendId: string) => {
+        socketService.emit(SOCKET_EVENTS.FRIEND.REMOVE_FRIEND, {
+            userId: currentUserId,
+            userFriendId: userFriendId,
+        })
+    }, [currentUserId]);
+
+    const accpetFriend = useCallback((userFriendId: string) => {
+        socketService.emit(SOCKET_EVENTS.FRIEND.ACCEPT_FRIEND, {
+            userId: currentUserId,
+            userFriendId: userFriendId,
+        });
+    }, [currentUserId]);
+
+    return {
+        listFriends,
+        receiveFriends,
+        sendFriends,
+        inviteFriend,
+        removeFriend,
+        revokeInviteFriend,
+        accpetFriend,
+        getListFriends,
+        getReceviedInviteFriends,
+        getSendListFriends,
+        rejectInviteFriend,
+    };
+
+}
--- src/redux/userSlice.ts
+++ src/redux/userSlice.ts
@@ -1,10 +1,13 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { User } from '@/models/user';
 import { RootState } from '@/redux/store';
-const initialState: { user: User | null, accessToken: String | null, refreshToken: String | null } = {
+const initialState: { user: User | null, accessToken: String | null, refreshToken: String | null, sendFriends: any, receiveFriends: any, friends: any } = {
     user: null,
     accessToken: null,
     refreshToken: null,
+    sendFriends: [],
+    receiveFriends: [],
+    friends: [],
 };
 const userSlice = createSlice({
     name: 'user',
@@ -30,11 +33,21 @@ const userSlice = createSlice({
             state.accessToken = null;
             state.refreshToken = null;
             console.log('clearTokens thành công', state.accessToken, state.refreshToken);
-        }
+        },
+        setFriends: (state: any, { payload }: PayloadAction<any>) => {
+            state.friends = payload;
+        },
+        setSendFriends: (state: any, { payload }: PayloadAction<any>) => {
+            state.sendFriends = payload;
+        },
+        setReceiveFriends: (state: any, { payload }: PayloadAction<any>) => {
+            state.receiveFriends = payload;
+        },
+
     },
 });
 
-export const { setMe, clearUser, setAccessToken, setRefreshToken, clearTokens } = userSlice.actions;
+export const { setMe, clearUser, setAccessToken, setRefreshToken, clearTokens, setReceiveFriends, setFriends, setSendFriends } = userSlice.actions;
 
 
 
